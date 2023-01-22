d7a06789a48842e78d300663edb0c42ca2f5dd54
MacRimi <123239993+MacRimi@users.noreply.github.com>
2023-01-22 16:40:43 +0100

    Update desactivar_idioma_defecto.sh

--- desactivar_idioma_defecto.sh
+++ desactivar_idioma_defecto.sh
@@ -1,27 +1,54 @@
-#!/bin/sh
-# Desactivar el idioma por defecto
-    echo ""
-    echo ""
+#!/usr/bin/env bash
+function header_info {
+cat <<"EOF"
+ ____                           ___  _
+|  _ \ _ __ _____  ___ __ ___  / _ \| | ___   __ _ _   _ 
+| |_) | '__/ _ \ \/ / '_ ` _ \| | | | |/ _ \ / _` | | | |
+|  __/| | | (_) >  <| | | | | | |_| | | (_) | (_| | |_| |
+|_|   |_|  \___/_/\_\_| |_| |_|\___/|_|\___/ \__, |\__, |
+                                             |___/ |___/ 
+                                             
+                                             
+EOF
+}
+clear
+header_info
 
-if [ -f "/etc/apt/apt.conf.d/30desdability-lang" ];then
 
-    echo "-------------------------------------------"
-    echo "* El idioma por decto ya esta desactivado *"
-    echo "-------------------------------------------"
 
-    echo ""
-    echo ""
-else
+txtrst=$(tput sgr0) 
+txtred=$(tput setab 1) 
+textpurple=$(tput setab 5) 
+txtgreen=$(tput bold ; tput setaf 2)
 
-    echo -e "Acquire::Languages \"none\"; Acquire::IndexTargets::deb::Contents-deb::DefaultEnabled \"false\";\\n" > /etc/apt/apt.conf.d/30desdability-lang
 
-    echo "------------------------------------------"
-    echo "* El dioma por defecto se ha desactivado *"
-    echo "------------------------------------------"
-
-    echo ""
-    echo ""
-    echo "Reinicia proxmox para aplicar el cambio"
-    echo ""
-    echo ""
+PS3="${txtgreen}Desea quitar cartel suscripcion proxmox? ${txtrst}"
+select suscripcion in SI NO
+do
+	break;
+done
+if [ "$suscripcion" = "SI" ]; then
+  cp /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js.old
+  sed 's/if (data.status !== 'Active') {/if (false) {/' /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js
+  systemctl restart pveproxy.service
+echo "suscripcion quitada."
+echo ""
+echo ""
+echo ""
+	
+fi
+PS3="${txtgreen}Desea hacer update ? ${txtrst}"
+select update in SI NO
+do
+	break;
+done
+if [ "$update" = "SI" ]; then	
+	
+	apt-get update
+clear
+echo "update realizado con exito."
+echo ""
+echo ""
+echo ""
+echo ""
 fi
